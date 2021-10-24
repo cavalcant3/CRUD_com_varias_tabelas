fcb428ff60e06776b4a70b71d5804bdaf1fa92dc
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-24 15:17:01 -0300

    definindo atributos da tabela produto

---
+++ exemplo crud/exemplo-crud/src/main/java/crud/example/exemplo/crud/domain/Produto.java
@@ -0,0 +1,23 @@
+package crud.example.exemplo.crud.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private Double valor;
+
+}

e3738b22b85475eaaba9c1a1c1bdeee8c927cc02
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-27 02:10:21 -0300

    ajustes

--- demo (1)/demo/src/main/java/crud10/example/demo/student/StudentController.java
+++ demo (1)/demo/src/main/java/crud10/example/demo/student/StudentController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
@@ -31,8 +33,16 @@ public class StudentController {
         studentService.deleteStudent(studentId);
     }
 
+   // @PutMapping(path = "{studentId}")
+  //  public void updateStudent(@RequestBody Student student) {
+   //     studentService.replace(student);
+   // } meu metodo de resolver, o metodo correto:
     @PutMapping(path = "{studentId}")
-    public void updateStudent(@RequestBody Student student) {
-        studentService.replace(student);
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email){
+        studentService.updateStudent(studentId, name, email);
     }
 }
+
--- demo (1)/demo/src/main/java/crud10/example/demo/student/StudentService.java
+++ demo (1)/demo/src/main/java/crud10/example/demo/student/StudentService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -43,8 +45,32 @@ public class StudentService {
        
     }
 
-    public void replace(Student student) {
-        deleteStudent(student.getId());
-        studentRepistory.save(student);
+    //@Transactional
+   // public void replace(Student student) {
+   //     deleteStudent(student.getId());
+   //     studentRepistory.save(student);
+ //   }
+
+    @Transactional
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email) {
+        Student student = studentRepistory.findById(studentId)
+                .orElseThrow(()-> new IllegalStateException(
+                        "student with id" + studentId + "does not exists"
+                ));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+        if(email != null &&
+             email.length()> 0 &&
+        !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepistory
+                    .findStudentByEmail(email);
+            if (studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
